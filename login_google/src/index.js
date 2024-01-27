ce2ebbe32fd2f655f1d7f562db6018fdb7b192ac
hahuyhungdev <hahuyhungdev@gmail.com>
2024-01-27 12:13:47 +0700

    add feat login oauth

---
+++ login_google/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId='600083102854-c7u14hn0s710p11n7bato916475fh8v2.apps.googleusercontent.com'>
+      <App />
+    </GoogleOAuthProvider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();

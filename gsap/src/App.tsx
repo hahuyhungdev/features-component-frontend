68009b6c3382913ace9fc68241f7781b688f1f01
hahuyhungdev <hahuyhungdev@gmail.com>
2024-02-07 23:36:44 +0700

    animation with gsap content page hero

--- gsap/src/App.tsx
+++ gsap/src/App.tsx
@@ -1,13 +1,15 @@
 // import './App.css';
 import './App.scss';
 import Circles from './Circles';
+import Hero from './components/Hero';
 import ImageRevealEffect from './components/imageRevealEffect';
 
 function App() {
   return (
     <>
       {/* <Circles /> */}
-      <ImageRevealEffect />;
+      {/* <ImageRevealEffect />; */}
+      <Hero />
     </>
   );
 }

dd5f2224ed8a4a636eb7a9cc947620a7ebd2e082
hahuyhungdev <hahuyhungdev@gmail.com>
2024-02-07 23:36:53 +0700

    add template and feat menu select box

---
+++ Demo/src/components/icons/arrowLeftOutlineIcon/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { IICon } from '../../../types/icons'
+
+export const ArrowLeftOutlineIcon = ({ width = 22, height = 18 }: IICon) => {
+  return (
+    <svg width={width} height={height} viewBox='0 0 22 18' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path d='M10 17L2 9L10 0.999999' stroke='black' strokeWidth='1.5' />
+      <path d='M2 9H22' stroke='black' strokeWidth='1.5' />
+    </svg>
+  )
+}
---
+++ Demo/src/components/icons/arrowRight/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { IICon } from '../../../types/icons'
+
+export const ArrowRight = ({ width = 17, height = 28, className }: IICon) => {
+  return (
+    <svg
+      width={width}
+      height={height}
+      className={className}
+      viewBox='0 0 17 28'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M0 3.052L10.406 13.852L0 24.645L2.943 27.698L16.293 13.852L2.943 0L0 3.052Z'
+        fill='#B1B7C4'
+      />
+    </svg>
+  )
+}

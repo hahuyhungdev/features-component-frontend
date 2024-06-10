6d6b0f583f17b6c60ae77535c295a2b6ee08b3d5
hahuyhungdev <hahuyhungdev@gmail.com>
2024-06-10 14:44:42 +0700

    feat: implement component rank

--- Slick/src/App.tsx
+++ Slick/src/App.tsx
@@ -4,18 +4,20 @@ import SliderCarousel from './components/SliderCarousel'
 import Slider, { type Settings } from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
-import Main from './imgs/main.jpg'
-import Main1 from './imgs/main1.jpg'
-import Main2 from './imgs/main2.jpg'
 
 import SliderButton from 'components/SliderButton'
-import { ArrowLeftOutlineIcon, ArrowRightOutlineIcon, RectangleIcon } from 'components/icons/'
-
-interface SliderItem {
-  img: string
-  title: string
-  description: string
-}
+import {
+  ArrowLeftOutlineIcon,
+  ArrowRight,
+  ArrowRightOutlineIcon,
+  ArrowUp,
+  RankingIcon,
+  RectangleIcon
+} from 'components/icons/'
+import { dataBanner, dataListCard, dataValue } from '_mockdata'
+import top1 from './imgs/top1.png'
+import top2 from './imgs/top2.png'
+import top3 from './imgs/top3.png'
 
 function App() {
   const settings: Settings = {
@@ -74,85 +76,7 @@ function App() {
     ]
   }
   const { prevArrow, nextArrow, customPaging, appendDots, ...restSettings } = settings
-  const dataBanner: SliderItem[] = [
-    {
-      img: Main,
-      title: 'Main',
-      description: 'descridescriptiondescriptionption'
-    },
-    {
-      img: Main1,
-      title: 'Main1',
-      description: 'descridescriptiondescriptionption'
-    },
-    {
-      img: Main2,
-      title: 'Main3',
-      description: 'descridescriptiondescriptionption'
-    },
-    {
-      img: Main1,
-      title: 'Main4',
-      description: 'descridescriptiondescriptionption'
-    },
-    {
-      img: Main1,
-      title: 'Main5',
-      description: 'descridescriptiondescriptionption'
-    }
-  ]
-  const dataListCard: SliderItem[] = [
-    {
-      img: Main,
-      title: 'Main',
-      description: 'descridescriptiondescriptionption'
-    },
-    {
-      img: Main1,
-      title: 'Main1',
-      description: 'descridescriptiondescriptionption'
-    },
-    {
-      img: Main2,
-      title: 'Main3',
-      description: 'descridescriptiondescriptionption'
-    },
-    {
-      img: Main2,
-      title: 'Main4',
-      description: 'descridescriptiondescriptionption'
-    },
-    {
-      img: Main1,
-      title: 'Main5',
-      description: 'descridescriptiondescriptionption'
-    },
-    {
-      img: Main,
-      title: 'Main',
-      description: 'descridescriptiondescriptionption'
-    },
-    {
-      img: Main1,
-      title: 'Main1',
-      description: 'descridescriptiondescriptionption'
-    },
-    {
-      img: Main,
-      title: 'Main3',
-      description: 'descridescriptiondescriptionption'
-    },
-    {
-      img: Main1,
-      title: 'Main4',
-      description: 'descridescriptiondescriptionption'
-    },
-    {
-      img: Main1,
-      title: 'Main5',
-      description: 'descridescriptiondescriptionption'
-    }
-  ]
+
   const handleCLick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault()
     console.log('click')
@@ -206,6 +130,101 @@ function App() {
             </div>
           ))}
         </Slider>
+      </div>
+      <div className='ranking'>
+        <div className='title'>
+          <h1>넥슨 크리에이터즈 랭킹</h1>
+          <ArrowRight />
+        </div>
+        <div className='columns'>
+          <div className='type1'>
+            {dataValue.map((item, index) => (
+              <div
+                className='item'
+                key={index}
+                style={{
+                  // check if top 1,2,3 then background img is top1,2,3. outtop 4 then not show background img
+                  backgroundImage: `url(${
+                    item.rank === 1 ? top1 : item.rank === 2 ? top2 : item.rank === 3 ? top3 : ''
+                  })`,
+                  width: `${
+                    item.rank === 1 ? '404px' : item.rank === 2 ? '365px' : item.rank === 3 ? '331px' : '100%'
+                  }`,
+                  height: `${item.rank <= 3 ? '104px' : '60px'}` // add this line to set the height
+                }}
+              >
+                {item.rank <= 3 ? (
+                  <>
+                    <div className='content'>
+                      <div className='rank'>{item.rank}</div>
+                      <img src={item.img} alt={item.label} />
+                      <div className='valueContent'>
+                        <p className='label'>{item.label}</p>
+                        <p className='description'>{item.description}</p>
+                      </div>
+                    </div>
+                    <div className='last_content'>
+                      <div className='change'>
+                        <ArrowUp />
+                        <span>{item.changeRank}</span>
+                      </div>
+
+                      <div className='value'>{item.percent}</div>
+                    </div>
+                  </>
+                ) : (
+                  <div className='newValue'>
+                    <div className='rank'>{item.rank}</div>
+                    <div className='substance'>
+                      <span>NEW</span>
+                      <p className='label'>{item.label}</p>
+                      <p className='description'>{item.description}</p>
+                      <div className='value'>{item.percent}</div>
+                    </div>
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+          <div className='type2'>
+            {dataValue.map((item, index) => (
+              <div
+                className='item'
+                key={index}
+                style={
+                  {
+                    // check if top 1,2,3 then background img is top1,2,3. outtop 4 then not show background img
+                  }
+                }
+              >
+                {item.rank <= 3 ? (
+                  <div className='newValue'>
+                    <div className='rank'>{item.rank}</div>
+                    <div className='change'>
+                      <ArrowUp />
+                      <span>{item.changeRank}</span>
+                    </div>
+                    <div className='substance'>
+                      <p className='label'>{item.label}</p>
+                      <p className='description'>{item.description}</p>
+                      <div className='value'>{item.percent}</div>
+                    </div>
+                  </div>
+                ) : (
+                  <div className='newValue'>
+                    <div className='rank'>{item.rank}</div>
+                    <div className='substance'>
+                      <span>NEW</span>
+                      <p className='label'>{item.label}</p>
+                      <p className='description'>{item.description}</p>
+                      <div className='value'>{item.percent}</div>
+                    </div>
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   )

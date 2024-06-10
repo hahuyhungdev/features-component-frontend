e7a528b71803ba607ca5701be1e4179460f22750
hahuyhungdev <hahuyhungdev@gmail.com>
2024-06-10 14:44:34 +0700

    gsap project basic

---
+++ gsap/src/Circles.tsx
@@ -0,0 +1,68 @@
+import gsap, { Power3 } from 'gsap';
+import { useEffect, useRef } from 'react';
+import './App.css';
+
+function Circles() {
+  let circleYellow = useRef<HTMLDivElement | null>(null);
+  let circleRed = useRef<HTMLDivElement | null>(null);
+  let circleBlue = useRef<HTMLDivElement | null>(null);
+  let tl = gsap.timeline({ repeat: -1, repeatDelay: 1 });
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      // gsap.from(circleYellow.current, { x: 100, duration: 2, ease: Power3.easeOut });
+      // gsap.from(circleRed.current, { x: 40, duration: 1, ease: Power3.easeOut, delay: 0.2 });
+      // gsap.from(circleBlue.current, { x: 40, duration: 1, ease: Power3.easeOut, delay: 0.4 });
+      tl.to([circleYellow.current, circleRed.current, circleBlue.current], {
+        x: 100,
+        duration: 0.8,
+        ease: Power3.easeOut,
+        cursor: 'pointer',
+        stagger: {
+          amount: 1.5,
+          // from: 'center',
+          grid: 'auto',
+          ease: 'power3.inOut',
+          repeat: 1,
+          yoyo: true,
+          each: 0.2,
+        },
+      });
+    });
+    return () => ctx.revert(); // <-- CLEANUP!
+  }, [tl]);
+
+  const handleExpand = (name: React.MutableRefObject<HTMLDivElement | null>) => {
+    gsap.to(name.current, { height: 200, width: 200, duration: 1, ease: Power3.easeOut });
+  };
+  const handleShrink = (name: React.MutableRefObject<HTMLDivElement | null>) => {
+    gsap.to(name.current, { height: 75, width: 75, duration: 1, ease: Power3.easeOut });
+  };
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <div className='cirle-container'>
+          <div
+            ref={circleYellow}
+            className='circle yellow'
+            onMouseEnter={() => handleExpand(circleYellow)}
+            onMouseLeave={() => handleShrink(circleYellow)}
+          ></div>
+          <div
+            ref={circleRed}
+            onMouseEnter={() => handleExpand(circleRed)}
+            onMouseLeave={() => handleShrink(circleRed)}
+            className='circle red'
+          ></div>
+          <div
+            ref={circleBlue}
+            className='circle blue'
+            onMouseEnter={() => handleExpand(circleBlue)}
+            onMouseLeave={() => handleShrink(circleBlue)}
+          ></div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Circles;
---
+++ gsap/src/basic.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useRef } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import gsap, { Power3 } from 'gsap';
+
+function Basic() {
+  let logoItem = useRef<HTMLImageElement | null>(null);
+  let textItem = useRef<HTMLParagraphElement | null>(null);
+
+  useEffect(() => {
+    console.log(logoItem);
+    gsap.to(logoItem.current, { opacity: 1, duration: 3, y: -50, ease: Power3.easeOut });
+    gsap.to(textItem.current, { opacity: 1, delay: 0.2, y: -50, ease: Power3.easeOut });
+  }, []);
+
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img ref={logoItem} src={logo} className='App-logo' alt='logo' />
+        <p ref={textItem}>Edist src/App.tsx and save to reldoad.</p>
+        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Basic;
